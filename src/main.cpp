3d1eed924297d7b5d1fff86ffb55226c3bb9a963
Benjamin Low <lthben@gmail.com>
2019-11-02 23:48:41 +0800

    changed order of sculptures ID

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
       Teensy 3.2 with audio shield. 
       Sculpture 1, 2 and 3 have two buttons and one distance sensor each.
       The two buttons play back the two sets of air measurements readings translated into brightness values. 
-      There is one strip of leds per set of reading, therefore two strips per sculpture.
+      One button represents one set of reading and one strip of led.
       There is an idle mode pulsing light animation. Active mode is triggered by button and will show a sequence of brightness values. 
       The distance sensor changes the hue of the leds in real time. 
       There is a sound for idle mode and one for active playback mode. 
@@ -24,18 +24,20 @@
 //-------------------- USER DEFINED SETTINGS --------------------//
 
 //Uncomment one below
-#define __PM25__ 
 // #define __CO2__
+#define __PM25__ 
 // #define __VOC__
+
+const int CO2_1[17] = { 1609, 577, 406, 419, 443, 414, 403, 413, 409, 411, 412, 409, 423, 414, 421, 434, 421 };
+const int CO2_2[40] = { 1685, 642, 618, 698, 697, 778, 450, 664, 648, 676, 425, 504, 550, 481, 640, 942, 1791, 504, 733, 688, 592, 608, 850, 779, 1876, 646, 648, 659, 893, 422, 455, 701, 716, 892, 1046, 455, 483, 503, 448, 550 };
 
 const int PM25_1[20] = { 118, 38, 34, 111, 125, 82, 178, 174, 43, 43, 42, 83, 63, 83, 85, 103, 68, 53, 54, 66 };
 const int PM25_2[32] = { 65, 88, 44, 42, 73, 69, 70, 61, 54, 89, 86, 91, 60, 63, 92, 88, 95, 55, 85, 49, 48, 51, 35, 38, 49, 51, 21, 32, 28, 42, 21, 25 };
-const int CO2_1[17] = { 1609, 577, 406, 419, 443, 414, 403, 413, 409, 411, 412, 409, 423, 414, 421, 434, 421 };
-const int CO2_2[40] = { 1685, 642, 618, 698, 697, 778, 450, 664, 648, 676, 425, 504, 550, 481, 640, 942, 1791, 504, 733, 688, 592, 608, 850, 779, 1876, 646, 648, 659, 893, 422, 455, 701, 716, 892, 1046, 455, 483, 503, 448, 550 };
+
 const int VOC_1[26] = { 8, 11, 5, 13, 16, 14, 15, 17, 15, 20, 29, 21, 22, 19, 14, 13, 19, 25, 17, 15, 13, 17, 16, 15, 20, 17 };
 const int VOC_2[22] = { 122, 67, 24, 36, 46, 32, 29, 34, 27, 25, 22, 23, 19, 23, 21, 33, 26, 34, 41, 15, 25, 18 };
 
-const int PM25band1 = 50, PM25band2 = 50, CO2band1 = 50, CO2band2 = 50, VOCband1 = 50, VOCband2 = 50; //num of pixels per strip. Each pixel is 10cm.
+const int CO2band1 = 50, CO2band2 = 50, PM25band1 = 50, PM25band2 = 50, VOCband1 = 50, VOCband2 = 50; //num of pixels per strip. Each pixel is 10cm.
 
 CHSV cblue(140,255,255);
 const int BAND_DELAY = 500;   //controls led animation speed
@@ -45,9 +47,9 @@ const int BAND_DELAY = 500;   //controls led animation speed
 Bounce button0 = Bounce(0, 15); // 15 = 15 ms debounce time
 Bounce button1 = Bounce(1, 15);
 
-bool isButtonPressed = false; //track response to button triggered
+bool isButton0Pressed, isButton1Pressed; //track response to button triggered
 
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox = Adafruit_VL53L0X(); //SDL to 19 and SDA to 18
 int rangeVal;
 elapsedMillis loxmsec; //to track that it takes measurement at an interval of around 100ms instead of continuously
 
@@ -58,17 +60,18 @@ elapsedMillis loxmsec; //to track that it takes measurement at an interval of ar
 #define LED_TYPE UCS1903
 #define COLOR_ORDER GRB //Yes! GRB!
 
-CHSV myColor = cblue;
+CHSV strip1Color = cblue;
+CHSV strip2Color = cblue;
 
-#if defined(__PM25__)
-const int BAND1 = PM25band1, BAND2 = PM25band2; 
-const int SCULPTURE_ID = 1;
-int readings1[20], readings2[32];
-
-#elif defined(__CO2__)
+#if defined(__CO2__)
 const int BAND1 = CO2band1, BAND2 = CO2band2;
-const int SCULPTURE_ID = 2;
+const int SCULPTURE_ID = 1;
 int readings1[17], readings2[40];
+
+#elif defined(__PM25__)
+const int BAND1 = PM25band1, BAND2 = PM25band2; 
+const int SCULPTURE_ID = 2;
+int readings1[20], readings2[32];
 
 #elif defined(__VOC__)
 const int BAND1 = VOCband1, BAND2 = VOCband2;
@@ -79,20 +82,20 @@ int readings1[26], readings2[22];
 #error "invalid sculpture ID"
 #endif
 
-CRGB leds0[BAND1], leds1[BAND2];
 #define UPDATES_PER_SECOND 100 //speed of light animation
-
-int brightness1, brightness2; //band brightness
-int maxBrightLvl = 255; //variable max brightness
 const int IDLE_MODE = 1, BUTTON_MODE = 2;
-unsigned int playMode = IDLE_MODE; 
-bool hasplayModeChanged; //for audio track changes
-int activeLedState = 0;            //to track led animaton states, e.g. 0 - idle mode, start fade to black 1 - show brightness according to reading, 2 - has completed animations, fade to black and idle
-bool isMaxBrightness = false;      //to track idle animation direction
-elapsedMillis bandms;              //multiple use time ellapsed counter
-unsigned int band_delay = BAND_DELAY; //speed of fade animation
-unsigned int readingsCounter;                 //keeps track of indexing the readings array
-unsigned int prevBrightVal, currBrightVal;    //for comparing prev and current values for dimming and brightening
+
+CRGB leds0[BAND1]; CRGB leds1[BAND2];
+int strip1brightness, strip2brightness; //band brightness
+int strip1maxBrightLvl = 255, strip2maxBrightLvl = 255; //variable max brightness
+unsigned int strip1playMode = IDLE_MODE, strip2playMode = IDLE_MODE; 
+bool strip1hasPlayModeChanged, strip2hasplayModeChanged; //for audio track changes
+int strip1activeLedState = 0, strip2activeLedState;            //to track led animaton states, e.g. 0 - idle mode, start fade to black 1 - show brightness according to reading, 2 - has completed animations, fade to black and idle
+bool strip1isMaxBrightness, strip2isMaxBrightness;      //to track idle animation direction
+elapsedMillis strip1bandms, strip2bandms;              //multiple use time ellapsed counter
+unsigned int strip1bandDelay = BAND_DELAY, strip2bandDelay = BAND_DELAY; //speed of fade animation
+unsigned int strip1readingsCounter, strip2readingsCounter;                 //keeps track of indexing the readings array
+unsigned int strip1prevBrightVal, strip1currBrightVal, strip2prevBrightVal, strip2currBrightVal;    //for comparing prev and current values for dimming and brightening
 
 #include "myfunctions.h" //supporting functions
 
@@ -130,5 +133,21 @@ void setup()
 void loop() 
 {
   read_console();
+
+  check_playMode();
+
+  if (strip1playMode == IDLE_MODE)
+  {
+    strip1_fade_animation();
+  }
+  else if (strip1playMode == BUTTON_MODE)
+  {
+    strip1_playback_readings(); //play brightness sequence according to readings[] array
+  }
+
+  
+
+  FastLED.show();
+  FastLED.delay(1000 / UPDATES_PER_SECOND);
 }
 
--- src/myfunctions.h
+++ src/myfunctions.h
@@ -9,12 +9,12 @@ void read_console()
 
   if (button0.fallingEdge())
   {
-    isButtonPressed = true;
+    isButton0Pressed = true;
     Serial.println("button0 pressed");
   }
   if (button1.fallingEdge())
   {
-    isButtonPressed = true;
+    isButton1Pressed = true;
     Serial.println("button1 pressed");
   }
 
@@ -22,17 +22,17 @@ void read_console()
   {
     VL53L0X_RangingMeasurementData_t measure;
 
-    Serial.print("Reading a measurement... ");
+    // Serial.print("Reading a measurement... ");
     lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
     if (measure.RangeStatus != 4) 
     {  // phase failures have incorrect data
       int rangeVal = measure.RangeMilliMeter;
-      Serial.print("Distance (mm): "); Serial.println(rangeVal);
+      // Serial.print("Distance (mm): "); Serial.println(rangeVal);
     } 
     else 
     {
-      Serial.println(" out of range ");
+      // Serial.println(" out of range ");
     }
 
     loxmsec = 0; //refresh timer for next reading
@@ -46,6 +46,17 @@ void register_readings()
 {
   if (SCULPTURE_ID == 1)
   {
+    for (int i = 0; i < 17; i++)
+    {
+      readings1[i] = CO2_1[i];
+    }
+    for (int i = 0; i < 40; i++)
+    {
+      readings2[i] = CO2_2[i];
+    }
+  }
+  else if (SCULPTURE_ID == 2)
+  {
     for (int i = 0; i < 20; i++)
     {
       readings1[i] = PM25_1[i];
@@ -54,17 +65,6 @@ void register_readings()
     {
       readings2[i] = PM25_2[i];
     }
-  }
-  else if (SCULPTURE_ID == 2)
-  {
-      for (int i = 0; i < 17; i++)
-      {
-        readings1[i] = CO2_1[i];
-      }
-      for (int i = 0; i < 40; i++)
-      {
-        readings2[i] = CO2_2[i];
-      }
   }
   else //sculpture 3
   {
@@ -77,4 +77,143 @@ void register_readings()
       readings2[i] = VOC_2[i];
     }
   }
+}
+
+/*--------------------------------------------------------------------------------
+  Toggles the playMode according to button press
+--------------------------------------------------------------------------------*/
+void check_playMode() 
+{
+  if (isButton0Pressed == true) //process button press
+  {
+    isButton0Pressed = false; //listen again for button presses
+    strip1playMode = BUTTON_MODE;
+    strip1hasPlayModeChanged = true; //trigger sound change
+    Serial.println("strip1 : BUTTON MODE");
+
+    strip1activeLedState = 0;          //reset the led if currently active
+    strip1bandDelay = BAND_DELAY / 4; //speed up the fade animation
+  }
+}
+
+/*--------------------------------------------------------------------------------
+  led strip functions
+--------------------------------------------------------------------------------*/
+void fade_strip1()
+{
+  for (int i = 0; i<BAND1; i++)
+  {
+    leds0[i].fadeToBlackBy(8);
+  }
+}
+
+void fade_strip2()
+{
+  for (int i = 0; i<BAND2; i++)
+  {
+    leds1[i].fadeToBlackBy(8);
+  }
+}
+
+void set_strip1_brightLevel (int brightlvl)
+{
+  strip1Color.val = brightlvl;
+
+  for (int i=0; i<BAND1; i++)
+  {
+    leds0[i] = strip1Color;
+  }
+}
+
+void set_strip2_brightLevel (int brightlvl)
+{
+  strip2Color.val = brightlvl;
+
+  for (int i=0; i<BAND2; i++)
+  {
+    leds1[i] = strip2Color;
+  }
+}
+
+bool has_strip1_fade()
+{
+  if (leds0[0].getAverageLight() == 0 && leds0[BAND1].getAverageLight() == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool has_strip2_fade()
+{
+  if (leds1[0].getAverageLight() == 0 && leds1[BAND2].getAverageLight() == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+/*--------------------------------------------------------------------------------
+  Tracks fade animation bright levels
+--------------------------------------------------------------------------------*/
+int strip1_get_brightness(int _brightness)
+{
+  if (!strip1isMaxBrightness)
+  {
+    _brightness++;
+    if (_brightness > strip1maxBrightLvl)
+      _brightness = strip1maxBrightLvl;
+    return _brightness;
+  }
+  else //reached max brightness
+  {
+    _brightness--;
+    if (_brightness < 0)
+      _brightness = 0;
+    return _brightness;
+  }
+}
+
+/*--------------------------------------------------------------------------------
+  Idle fade animation
+--------------------------------------------------------------------------------*/
+void strip1_fade_animation()
+{
+  int brightlevel = strip1_get_brightness(strip1brightness);
+  strip1Color.val = brightlevel;
+  strip1brightness = brightlevel;
+  for (int i=0; i<BAND1; i++)
+  {
+    leds0[i] = strip1Color;
+  }
+  if (brightlevel == strip1maxBrightLvl)
+  {
+    strip1isMaxBrightness = true;
+    strip1bandms = 0;
+  }
+  if (brightlevel == 0)
+  {
+    strip1isMaxBrightness = false;
+    strip1bandms = 0;
+  }
+  // Serial.println(strip1Color.val);
+}
+
+void strip2_fade_animation()
+{
+
+}
+
+/*--------------------------------------------------------------------------------
+  plays back readings
+--------------------------------------------------------------------------------*/
+void strip1_playback_readings()
+{
+
 }
